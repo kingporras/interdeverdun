38d6b852355c8ac74a85d9444f418930f678aef3
kingporras <victorfifa723@gmail.com>
2025-06-09 00:16:30 +0200

    jugadores-data.js to jugadores-data.js

---
+++ js/jugadores-data.js
@@ -0,0 +1,13 @@
+export const plantilla = [
+  { id: "el-porras", nombre: "El Porras", dorsal: 9, apodo: "El Porras" },
+  { id: "shh", nombre: "SHH", dorsal: 18, apodo: "SHH" },
+  { id: "salo", nombre: "SALO", dorsal: 69, apodo: "SALO" },
+  { id: "ivan-a", nombre: "IVÁN A", dorsal: 16, apodo: "IVÁN A" },
+  { id: "dani", nombre: "Dani", dorsal: 10, apodo: "Dani" },
+  { id: "gera", nombre: "Gera", dorsal: 7, apodo: "Gera" },
+  { id: "rony", nombre: "Rony", dorsal: 11, apodo: "Rony" },
+  { id: "daud", nombre: "Daud", dorsal: 6, apodo: "Daud" },
+  { id: "el-madriles", nombre: "El Madriles", dorsal: 17, apodo: "Madriles" },
+  { id: "pablo", nombre: "Pablo", dorsal: 1, apodo: "Pablo" },
+  { id: "grizzi", nombre: "Grizzi", dorsal: 8, apodo: "Grizzi" }
+];
